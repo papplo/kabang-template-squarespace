f17d88f09a505836ced817398cbaad365a9f1d4c
Pablo Anttila <pablo.anttila@kabang.se>
2020-03-04 15:54:04 +0100

    Add some dependencies, a build step and frameworks to this build

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  important: true,
+  theme: {
+    fontFamily: {
+      display: ['Londrina Solid', 'sans-serif'],
+      body: ['system-ui','BlinkMacSystemFont','-apple-system','Segoe UI','Roboto','Oxygen','Ubuntu','Cantarell','Fira Sans','Droid Sans','Helvetica Neue','sans-serif']
+    }
+  },
+  
+}
