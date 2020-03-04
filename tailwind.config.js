503c321e4d20002fce9eba3145121d0de96eb466
Pablo Anttila <pablo.anttila@kabang.se>
2020-03-04 20:31:06 +0100

    did sum stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   important: true,
   theme: {
+    clr: {
+      contrast: '#F57D05',
+      primary: '#FFC222',
+      compliment: '#6FCADC',
+      bodytext: '#3C383D',
+      bgshade: '#FAFAFA'
+    },
     fontFamily: {
       display: ['Londrina Solid', 'sans-serif'],
       body: ['system-ui','BlinkMacSystemFont','-apple-system','Segoe UI','Roboto','Oxygen','Ubuntu','Cantarell','Fira Sans','Droid Sans','Helvetica Neue','sans-serif']
